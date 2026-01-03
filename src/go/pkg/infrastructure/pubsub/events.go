12bf7b4047a8a9f367970de02763f02b3fa56236
James King <ripixel@gmail.com>
2026-01-03 15:11:52 +0000

    feat: correctly marshal protobuf messages when setting CloudEvent data

--- src/go/pkg/infrastructure/pubsub/events.go
+++ src/go/pkg/infrastructure/pubsub/events.go
@@ -1,7 +1,11 @@
 package pubsub
 
 import (
+	"encoding/json"
+
 	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 )
 
 // NewCloudEvent creates a standardized CloudEvent v1.0
@@ -11,8 +15,24 @@ func NewCloudEvent(source, eventType string, data interface{}) (cloudevents.Even
 	e.SetType(eventType)
 	e.SetSource(source)
 
-	if err := e.SetData(cloudevents.ApplicationJSON, data); err != nil {
-		return e, err
+	// If data is a protobuf message, use protojson to ensure correct JSON formatting (e.g. timestamps as strings)
+	if msg, ok := data.(proto.Message); ok {
+		opts := protojson.MarshalOptions{
+			UseProtoNames: true,
+		}
+		bytes, err := opts.Marshal(msg)
+		if err != nil {
+			return e, err
+		}
+		// Wrap in json.RawMessage so it's not base64 encoded
+		if err := e.SetData(cloudevents.ApplicationJSON, json.RawMessage(bytes)); err != nil {
+			return e, err
+		}
+	} else {
+		// Fallback to standard JSON for non-proto types
+		if err := e.SetData(cloudevents.ApplicationJSON, data); err != nil {
+			return e, err
+		}
 	}
 
 	return e, nil
