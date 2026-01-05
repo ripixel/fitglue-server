3ed9b945b89b00913be20dfbd9f08adbb6329f49
James King <ripixel@gmail.com>
2026-01-05 18:52:57 +0000

    fix: link firebase hosting to app

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -8,3 +8,10 @@ data "google_firebase_web_app_config" "web" {
   provider   = google-beta
   web_app_id = google_firebase_web_app.web.app_id
 }
+
+resource "google_firebase_hosting_site" "default" {
+  provider = google-beta
+  project  = var.project_id
+  site_id  = var.project_id # Default site
+  app_id   = google_firebase_web_app.web.app_id
+}
